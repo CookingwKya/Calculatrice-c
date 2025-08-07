b48e7f3e0bf78c10dfb010605d03f8e6ba74bfd5
Abdoulaye <adiallo06102@gmail.com>
2025-08-07 03:02:06 +0000

    Fix: Remove duplicate main() function from parser.c
    - Fixed multiple definition error
    - parser.c now contains only parser functions
    - main.c contains the main application logic

--- src/parser.c
+++ src/parser.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> // Pour isdigit
-
-
-int main(){
-
-}
+#include <ctype.h>
 
 typedef enum {
-    TOKEN_NUMBER,    // si il s'agit d'un nombre
-    TOKEN_OPERATOR,  // si il s'agit d'un opérateur
-    TOKEN_END,       // il s'agit de la fin de l'expression
-    TOKEN_ERROR,     // si il s'agit d'un caractère inconnu
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_END,
+    TOKEN_ERROR,
 } TokenType;
 
 typedef struct {
-    TokenType type;     // Le TYPE (qu'est-ce que c'est)
-    double value;       // La VALEUR (si c'est un nombre)
-    char operator;      // L'OPÉRATEUR (si c'est un opérateur)
+    TokenType type;
+    double value;
+    char operator;
 } Token;
 
-// Fonction utilitaire manquante
 int is_operator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
@@ -28,45 +22,37 @@ int is_operator(char c) {
 Token get_next_token(const char **expression) {
     Token token;
     const char *expr = *expression;
-   
-    // Ignorer les espaces (CORRIGÉ)
+    
+    // Ignorer les espaces
     while (*expr == ' ') {
         expr++;
     }
     
-    // Fin de chaîne (CORRIGÉ)
+    // Fin de chaîne
     if (*expr == '\0') {
         token.type = TOKEN_END;
         *expression = expr;
         return token;
     }
     
-    // AJOUTÉ : Vérifier si c'est un nombre
+    // Nombre multi-chiffres
     if (isdigit(*expr)) {
+        char number_str[64];
+        int i = 0;
+        
+        // Lire tous les chiffres consécutifs
+        while (isdigit(*expr)) {
+            number_str[i++] = *expr++;
+        }
+        number_str[i] = '\0';
+        
         token.type = TOKEN_NUMBER;
-        token.value = *expr - '0';  // Conversion simple pour un chiffre
-        expr++;
+        token.value = atof(number_str);
         *expression = expr;
         return token;
     }
-    //si le caractère est un nombre et qu'il est précédé par un autre nombre ?
-   if (isdigit(*expr)) {
-    char number_str[64];     // Buffer pour accumuler les chiffres
-    int i = 0;
     
-    // Lire TOUS les chiffres consécutifs
-    while (isdigit(*expr)) {
-        number_str[i++] = *expr++;  // Copier le caractère ET avancer
-    }
-    number_str[i] = '\0';    // Terminer la chaîne
-    
-    token.type = TOKEN_NUMBER;
-    token.value = atof(number_str);  // Convertir la chaîne complète
-    *expression = expr;
-    return token;
-}
-
-    // Vérifier si c'est un opérateur (CORRIGÉ)
+    // Opérateur
     if (is_operator(*expr)) {
         token.type = TOKEN_OPERATOR;
         token.operator = *expr;
@@ -78,5 +64,5 @@ Token get_next_token(const char **expression) {
     // Caractère non reconnu
     token.type = TOKEN_ERROR;
     *expression = expr;
-    return token;  
+    return token;
 }
